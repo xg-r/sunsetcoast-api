e520731ea79ef2637fefda3c9d5122dad8f452dc
tanull <thk98k@gmail.com>
2026-03-03 17:10:14 +0900

    init

---
+++ src/main/kotlin/kr/sunsetcoast/SunsetcoastApiApplication.kt
@@ -0,0 +1,11 @@
+package kr.sunsetcoast
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SunsetcoastApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SunsetcoastApiApplication>(*args)
+}
---
+++ src/test/kotlin/kr/sunsetcoast/SunsetcoastApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.sunsetcoast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SunsetcoastApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
