ae64824195eee43214df1e3d6fb3e220c7f09345
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2026-03-15 21:54:15 +0900

    feat: add fetch option in RoomEntity

--- src/main/kotlin/kr/sunsetcoast/domain/room/vo/RoomEntity.kt
+++ src/main/kotlin/kr/sunsetcoast/domain/room/vo/RoomEntity.kt
@@ -2,6 +2,7 @@ package kr.sunsetcoast.domain.room.vo
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -28,7 +29,7 @@ class RoomEntity(
     @Column(nullable = false, length = 30)
     val status: String,
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "business_owner_id")
     val businessOwnerEntity: BusinessOwnerEntity,
 
